aa2b65c6d2680f8257a811dd177df471f794bbf8
hurtadoalejo <alejohg2911@gmail.com>
2026-05-02 11:35:01 -0500

    Initial commit

---
+++ src/main/java/com/proyectofinal/billeteravirtual/BilleteravirtualApplication.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.billeteravirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilleteravirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilleteravirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectofinal/billeteravirtual/BilleteravirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.billeteravirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilleteravirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
